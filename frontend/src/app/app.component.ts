f0930e5be6200d628fd34b3b166f4d99a95a94f3
trivuniceva <trivunic99@gmail.com>
2025-09-21 23:47:47 +0200

    osnova za front

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
+import {SideNavComponent} from './layout/side-nav/side-nav.component';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    NgIf,
+    NgClass,
+    RouterOutlet,
+    SideNavComponent,
+
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+
+  hiddenRoutes = ['/', '/login'];
+
+  constructor(private router: Router) {}
+
+  showSideNav(): boolean {
+    return !this.hiddenRoutes.includes(this.router.url);
+  }
 }
