b14eaf9c30bc0b4e9ca44fc80355740398a2a024
trivuniceva <trivunic99@gmail.com>
2025-09-22 00:47:17 +0200

    users module

--- src/main/java/backend/IbApplication.java
+++ src/main/java/backend/IbApplication.java
@@ -2,8 +2,11 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import usersModule.usersModule.UsersModuleApplication;
 
 @SpringBootApplication
+@Import(UsersModuleApplication.class)
 public class IbApplication {
 
     public static void main(String[] args) {
