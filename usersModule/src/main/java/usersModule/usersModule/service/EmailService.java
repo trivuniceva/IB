7b99460b3a83b61eebc745342801710b850a04f7
trivuniceva <trivunic99@gmail.com>
2025-10-02 22:06:24 +0200

    bug sa mejlom?????

--- usersModule/src/main/java/usersModule/usersModule/service/EmailService.java
+++ usersModule/src/main/java/usersModule/usersModule/service/EmailService.java
@@ -1,29 +1,24 @@
 package usersModule.usersModule.service;
 
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-
-
 
 @Service
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+//    private final JavaMailSender mailSender;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+//    public EmailService(JavaMailSender mailSender) {
+//        this.mailSender = mailSender;
+//    }
 
     public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject(subject);
+//        message.setText(body);
+//        mailSender.send(message);
     }
 }
